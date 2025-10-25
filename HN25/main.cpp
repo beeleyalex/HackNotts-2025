4429bee3e3d10a1e8f6c099ee67ac8ae1074e92c
Alex Beeley <psyab30@nottingham.ac.uk>
2025-10-25 16:47:12 +0100

    test

--- HN25/main.cpp
+++ HN25/main.cpp
@@ -148,7 +148,7 @@ int main()
         glm::mat4 model = glm::mat4(1.0f);
         //model = glm::translate(model, glm::vec3(0.0f, 0.0f, 2.0f));
         //model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(2.5f, 0.5f, 2.5f));
+        model = glm::scale(model, glm::vec3(2.5f, 0.6f, 2.5f));
         float time = glfwGetTime();
         //model = glm::rotate(model, glm::radians(3 * time), glm::vec3(1.0f, 0.3f, 0.2f));
 
