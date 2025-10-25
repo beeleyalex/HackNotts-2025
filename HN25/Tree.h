6afc4979bbacee067ddc70d24282c5bd33971500
Alex Beeley <psyab30@nottingham.ac.uk>
2025-10-25 19:34:08 +0100

    tree with asset!!!

---
+++ HN25/Tree.h
@@ -0,0 +1,91 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <glad/glad.h>
+#include "stb_image.h"
+#include <iostream>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+
+class Tree
+{
+public:
+	glm::vec3 pos;
+    glm::mat4 modelMatrix;
+
+    Tree(glm::vec3 position) : pos(position)
+    {
+        updateModelMatrix();
+    }
+
+    void init()
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(Tree::vertices), Tree::vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Tree::indices), Tree::indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        // still texture
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        stbi_set_flip_vertically_on_load(true);
+
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load("../../resources/textures/tree.jpg", &width, &height, &nrChannels, 0);
+
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+        else
+            std::cout << "Failed to load texture" << std::endl;
+
+        stbi_image_free(data);
+    }
+
+    void draw()
+    {
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
+
+    void updateModelMatrix()
+    {
+        modelMatrix = glm::mat4(1.0f);
+        modelMatrix = glm::translate(modelMatrix, pos);
+    }
+
+    static constexpr float vertices[] = {
+        // positions        // texture coords
+        0.5f,  0.5f, 0.0f,  1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f,   // bottom right
+       -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,   // bottom left
+       -0.5f,  0.5f, 0.0f,  0.0f, 1.0f    // top left 
+    };
+
+    static constexpr int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+private:
+	unsigned int VBO, VAO, EBO, texture;
+};
+
+#endif
